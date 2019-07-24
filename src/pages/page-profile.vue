70e15adc9fb5e1ffeeec9eb961401ebad3c7eed3
Jose Juan <pe.dominguezjosejuan@gmail.com>
2019-07-24 11:16:21 -0500

    feat(PRO): estructura basica

--- src/pages/page-profile.vue
+++ src/pages/page-profile.vue
@@ -17,16 +17,31 @@ export default {
 		display: flex;
 		justify-content: flex-start;
 		flex-wrap: wrap;
+		margin: auto;
+		max-width: 1142px;
+
+		@media (max-width: 500px) {
+			padding: 0 15px;
+		}
 	}
 
 	.profile-menu {
-		background-color: color(primary);
+		background-color: color(dark);
+		border-radius: 7px;
 		flex: 1 1 30%;
+		padding: 122px 90px 0;
+
+		@media (max-width: 500px) {
+			padding: 64px 76px 14px;
+		}
 	}
 
 	.profile-info {
-		background-color: color(secondary);
+		background-color: color(disabled);
+		box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.08);
 		flex: 1 1 70%;
+		height: 100%;
+		padding: 17px 44px;
 	}
 </style>
 
