dc2047e77221618a1ca97ac4ed6e17ef93e938e2
elenaJO <ele.lucero1993@gmail.com>
2019-07-25 11:38:01 -0500

    feat(MAQ): input buscador

--- src/pages/page-components.vue
+++ src/pages/page-components.vue
@@ -38,6 +38,14 @@
 				<product-card class="product-card"/>		
 			</section>
 		</v-layout>
+		<v-layout wrap>
+			<v-flex xs12>
+				input search
+			</v-flex>
+			<v-flex xs12>
+				<app-search />
+			</v-flex>
+		</v-layout>
 	</div>
 </template>
 
@@ -45,6 +53,7 @@
 	const appButton = () => import('@/components/shared/buttons/app-button');
 	const appInput = () => import('@/components/shared/inputs/app-input');
 	const productCard = () => import('@/components/products/product-card');
+	const appSearch = () => import('@/components/shared/inputs/app-input-search');
 
 	export default {
 		name: 'components',
@@ -52,6 +61,7 @@
 			appButton,
 			appInput,
 			productCard,
+			appSearch,
 		},
 	};
 </script>
