7e7f66a02658ab7e0d3b71b11d64ee3c1be2067f
Jose Juan <pe.dominguezjosejuan@gmail.com>
2019-09-11 12:06:36 -0500

    feat(HOM): tab del buscador

--- src/components/home/searcher.vue
+++ src/components/home/searcher.vue
@@ -1,6 +1,14 @@
 <template>
 	<div class="searcher-container">
-		<section class="tab"></section>
+		<section class="tab">
+			<div class="wrapper-btn">
+				<button type="button" class="tab-button tab-button-active">Auto</button>
+				<div class="active-btn" :style="`background-color: ${globalColors.primary}`"></div>
+			</div>
+			<div>
+				<button type="button" class="tab-button">Puerta Levadiza</button>
+			</div>
+		</section>
 		<section class="form-wrapper">
 			<form class="form">
 				<SelectInput transparent placeholder="Seleccione un año" class="search-select"/>
@@ -33,7 +41,6 @@
 		background-color: color(black);
 		border: 1px solid white;
 		border-radius: 7px;
-		padding: 10px 20px 25px;
 	}
 
 	.form {
@@ -41,6 +48,7 @@
 		display: flex;
 		justify-content: center;
 		flex-direction: column;
+		padding: 37px 20px 0;
 	}
 
 	.search-btn {
@@ -52,5 +60,37 @@
 		height: 30px;
 		margin-bottom: 15px;
 		width: 100%;
+	}
+
+	.tab {
+		align-items: center;
+		border-bottom: 1px solid white;
+		display: flex;
+		justify-content: space-between;
+		padding: 0 40px;
+	}
+
+	.wrapper-btn {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.active-btn {
+		height: 5px;
+		transform: translateY(3px);
+		width: 100%;
+	}
+
+	.tab-button {
+		background-color: transparent;
+		color: white;
+		font-family: font(demi);
+		height: 50px;
+		padding: 0 15px
+	}
+
+	.tab-button-active {
+		font-family: font(bold);
 	}
 </style>
