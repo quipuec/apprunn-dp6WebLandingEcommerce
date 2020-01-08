281c5dc707a7d343d32ce8bc39b158ce1f755df0
Jose Juan <pe.dominguezjosejuan@gmail.com>
2020-01-08 17:59:32 -0500

    feat(PRO): ajuste dinamico para mostrar texto (plano o html)

--- src/components/products/content.vue
+++ src/components/products/content.vue
@@ -1,10 +1,14 @@
 <template>
 	<div class="wrapper-content">
 		<div
-			v-if="content.description"
+			v-if="isHtml"
 			class="content-description"
-			v-html="content.description"
+			v-html="description"
 		></div>
+		<div
+			v-else
+			class="content-description"
+		>{{description}}</div>
 		<div v-if="content.urlImages">
 				<div v-for="(item, index) in content.urlImage" :key="index">
 					<img 
@@ -24,8 +28,25 @@
 	</div>
 </template>
 <script>
+
+function description() {
+	const html = this.content.description.includes('</');
+	this.isHtml = !!html;
+	return this.content.description;
+}
+
+function data() {
+	return {
+		isHtml: false,
+	};
+}
+
 export default {
 	name: 'content-text',
+	computed: {
+		description,
+	},
+	data,
 	props: {
 		content: {
 			type: Object,
