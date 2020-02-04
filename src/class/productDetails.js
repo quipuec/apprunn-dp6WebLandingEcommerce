61797828ccd61b2653f14c7e5510f14fe5bc252e
Jose Juan <pe.dominguezjosejuan@gmail.com>
2020-02-03 19:00:21 -0500

    feat(ECO): actualizacion en precios de conversiones

--- src/class/productDetails.js
+++ src/class/productDetails.js
@@ -111,9 +111,9 @@ class ProductDetails {
 		const priceList = this.selectedProduct.warehouseProduct.priceList[this.priceListId];
 		const { units, price, discount } = priceList;
 		const rightConversion = units[this.selectedProduct.unitSelected];
-		this.selectedProduct.price = rightConversion ? rightConversion.price : price;
+		this.selectedProduct.price = rightConversion ? rightConversion.salePrice : price;
 		if (discount) {
-			const priceDiscount = discount * this.selectedProduct.price;
+			const priceDiscount = (1 - (discount / 100)) * this.selectedProduct.price;
 			this.selectedProduct.priceDiscount = Number(priceDiscount.toFixed(2));
 		} else {
 			this.selectedProduct.priceDiscount = this.selectedProduct.price;
