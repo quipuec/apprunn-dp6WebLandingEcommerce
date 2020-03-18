9e4e9a3c949899bec73a08744097daf503e7d8b9
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-03-18 15:53:00 -0500

    feat(ECO): Cambiar cantidad a presentaciones en carrito de comrpas

--- src/pages/page-buy.vue
+++ src/pages/page-buy.vue
@@ -17,7 +17,11 @@
 						<img :src="logo.section" alt="logo del método de pago">
 						<h2 class="payment-section-title">PRODUCTOS </h2>
 					</div>
-					<product-in-car v-for="(product, indexProduct) in getProductToBuy" :key="indexProduct" :product="product"/>
+					<product-in-car
+						v-for="(product, indexProduct) in getProductToBuy"
+						:key="indexProduct"
+						:product="product"
+					/>
 					<div class="footter-products-buy">
 						<app-button
 							max-width="225px"
