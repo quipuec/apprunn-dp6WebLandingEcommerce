49b9846bcc081c931a902e5cd86c7bef47b68bc2
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-05-13 13:19:33 -0500

    feat(PAY): implementando datafast. avance

--- src/components/order/credit-card-payment.js
+++ src/components/order/credit-card-payment.js
@@ -4,13 +4,28 @@ const Paymentez = () => import('@/components/order/paymentsMethods/paymentez');
 const DataFast = () => import('@/components/order/paymentsMethods/data-fast');
 
 export default {
+	props: {
+		paymentsTypes: {
+			required: true,
+			type: Array,
+		},
+	},
 	render(h) {
-		let selectedPaymentMethods = null;
-		if (this.isPeru) {
-			selectedPaymentMethods = [h(VisaPeru), h(DataFast)];
-		} else {
-			selectedPaymentMethods = [h(Paymentez), h(DataFast)];
-		}
+		const options = {
+			visa: h(VisaPeru),
+			paymentez: h(Paymentez),
+			datafast: h(DataFast),
+		};
+		let selectedPaymentMethods = [h(DataFast)];
+		this.paymentsTypes.forEach((t) => {
+			const { code } = t;
+			selectedPaymentMethods = selectedPaymentMethods.concat(options[code]);
+		});
+		// if (this.isPeru) {
+		// 	selectedPaymentMethods = [h(VisaPeru), h(DataFast)];
+		// } else {
+		// 	selectedPaymentMethods = [h(Paymentez), h(DataFast)];
+		// }
 		return h('div', {}, selectedPaymentMethods);
 	},
 };
--- src/components/order/paymentsMethods/data-fast.vue
+++ src/components/order/paymentsMethods/data-fast.vue
@@ -1,11 +1,64 @@
 <template>
 	<div>
 		<h1>Data fast</h1>
+		<form
+			ref="data-fast"
+			data-brand="VISA MASTER DINERS AMEX DISCOVER"
+			:action="redirecUrl"
+		></form>
 	</div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
+
+function mounted() {
+	this.getTokenId();
+}
+
+async function getTokenId() {
+	const body = {
+		orderId: this.getOrderId,
+		commerceCode: this.getCommerceData.code,
+	};
+	const url = 'payment-transaction/dataweb/checkouts';
+	const { data: response } = await this.$httpSales.post(url, body);
+	this.checkoutId = response.id;
+	this.createDataFastForm();
+}
+
+function createDataFastForm() {
+	const dataFastForm = document.createElement('script');
+	const src = `https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=${this.checkoutId}`;
+	dataFastForm.setAttribute('src', src);
+	const form = this.$refs['data-fast'];
+	form.appendChild(dataFastForm);
+}
+
+function redirecUrl() {
+	return process.env.REDIRECT_URI;
+}
+
+function data() {
+	return {
+		checkoutId: null,
+	};
+}
+
 export default {
 	name: 'data-fast',
+	computed: {
+		...mapGetters([
+			'getCommerceData',
+			'getOrderId',
+		]),
+		redirecUrl,
+	},
+	data,
+	methods: {
+		createDataFastForm,
+		getTokenId,
+	},
+	mounted,
 };
 </script>
 <style lang="scss" scoped>
