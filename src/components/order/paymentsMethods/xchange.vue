b0deebc2492160e056a08915853412aa65592fd3
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-06-23 18:04:26 -0500

    feat(MAQ): archivo unico para xchange

---
+++ src/components/order/paymentsMethods/xchange.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+
+	</div>
+</template>
+<script>
+function created() {
+	const isDev = process.env.NODE_ENV === 'development';
+	const url = 'https://xchange.paybox.la/api/';
+	const eviroment = isDev ? 'sandbox.js' : 'checkout.js';
+	const [head] = document.getElementsByTagName('head');
+	const xchangeScript = document.createElement('script');
+	xchangeScript.setAttribute('src', `${url}${eviroment}`);
+	head.appendChild(xchangeScript);
+}
+
+export default {
+	name: 'xchange',
+	created,
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
