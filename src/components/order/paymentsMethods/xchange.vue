b7c15392ad47b159f637ed64449f42c489645529
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-06-24 20:18:09 -0500

    feat(INT): mostrar modal con formulario

--- src/components/order/paymentsMethods/xchange.vue
+++ src/components/order/paymentsMethods/xchange.vue
@@ -2,18 +2,30 @@
 	<div>
 		<div id="ButtonPaybox" class="xchange-btn"></div>
 		<div v-show="false">
-			<input type="text" id="PayboxRemail" value="xchange@mail.com">
-			<input type="text" id="PayboxSendmail" value="asd@mail.com">
-			<input type="text" id="PayboxRename" value="pepe">
-			<input type="text" id="PayboxSendname" value="jj">
-			<input type="text" id="PayboxAmount" value="100">
+			<!-- correo del usuario de la cuenta xchange -->
+			<input type="text" id="PayboxRemail" value="donuterapia@gmail.com">
+			<!-- correo del usuario que está comprando -->
+			<input type="text" id="PayboxSendmail" :value="getResponsible.email">
+			<!-- Nombre del usuario xchange -->
+			<input type="text" id="PayboxRename" value="Ivan">
+			<!-- Nombre del usuario que realiza el pedido -->
+			<input type="text" id="PayboxSendname" :value="getResponsible.fullname">
+			<!-- Monto del pedido -->
+			<input type="text" id="PayboxAmount" :value="getOrderInfo.total">
 		</div>
 	</div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
 
 export default {
 	name: 'xchange',
+	computed: {
+		...mapGetters([
+			'getOrderInfo',
+			'getResponsible',
+		]),
+	},
 };
 </script>
 <style lang="scss" scoped>
