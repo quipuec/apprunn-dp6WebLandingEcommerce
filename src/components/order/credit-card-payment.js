03f414a5c9a8b72bbdde02cdde177aa2ebeff5b6
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-07-29 15:42:00 -0500

    feat(INT): nueva estructura en medios de pago (por enlace y por boton)

--- src/components/order/credit-card-payment.js
+++ src/components/order/credit-card-payment.js
@@ -1,8 +1,42 @@
+import { LINK, BUTTON } from '@/shared/enums/paymentStrategy';
 
 const VisaPeru = () => import('@/components/order/paymentsMethods/visa-payment');
 const Paymentez = () => import('@/components/order/paymentsMethods/paymentez');
 const DataFast = () => import('@/components/order/paymentsMethods/data-fast');
-const Xchange = () => import('@/components/order/paymentsMethods/xchange');
+const LeadGods = () => import('@/components/order/paymentsMethods/leadgods');
+const PlaceToPay = () => {};
+
+const paymentLinkCreator = (h, gateway) => {
+	const linkOptions = {
+		leadgods: LeadGods,
+		placetopay: PlaceToPay,
+	};
+	let selectedLinks = [];
+	gateway.forEach((t) => {
+		const { code } = t;
+		selectedLinks = selectedLinks.concat(h(linkOptions[code]));
+	});
+	return h(
+		'div',
+		selectedLinks,
+	);
+};
+const paymentButtonCreator = (h, gateway) => {
+	const buttonOptions = {
+		visa: VisaPeru,
+		paymentez: Paymentez,
+		datafast: DataFast,
+	};
+	let selectedButtons = [];
+	gateway.forEach((t) => {
+		const { code } = t;
+		selectedButtons = selectedButtons.concat(h(buttonOptions[code]));
+	});
+	return h(
+		'div',
+		selectedButtons,
+	);
+};
 
 export default {
 	props: {
@@ -12,26 +46,18 @@ export default {
 		},
 	},
 	render(h) {
-		const options = {
-			visa: h(VisaPeru),
-			paymentez: h(Paymentez),
-			dataweb: h(DataFast),
-			xchange: h(Xchange),
-		};
-		let selectedPaymentMethods = [h(Xchange)];
-		this.paymentsTypes.forEach((t) => {
-			const { code } = t;
-			selectedPaymentMethods = selectedPaymentMethods.concat(options[code]);
-		});
-		return h(
-			'div',
-			{
-				style: {
-					display: 'grid',
-					gridGap: '10px',
-					gridTemplateColumns: 'repeat(auto-fit, minmax(180px, 1fr))',
-				},
-			},
-			selectedPaymentMethods);
+		if (this.paymentsTypes.length === 0) {
+			return null;
+		}
+		const paymentsMethos = [];
+		const link = this.paymentsTypes.find(p => p.code === LINK);
+		if (link) {
+			paymentsMethos.push(paymentLinkCreator(h, link.gateway));
+		}
+		const button = this.paymentsTypes.find(p => p.code === BUTTON);
+		if (button) {
+			paymentsMethos.push(paymentButtonCreator(h, button.gateway));
+		}
+		return h('div', paymentsMethos);
 	},
 };
---
+++ src/components/order/paymentsMethods/leadgods.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<h1>Leadgods</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Leadgods',
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
