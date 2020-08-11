d7145122c82e500f2703eb28983ac00492daf18f
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-08-10 19:17:52 -0500

    feat(INT): conflictos

--- src/components/shared/category/filters-category.vue
+++ src/components/shared/category/filters-category.vue
@@ -23,8 +23,8 @@
 				/>
 			</div>
 		</div>
-		<div class="content-filter" v-if="contentFilters">
-			<p class="text-price">Precio</p>
+		<div class="content-filter mt-4" v-if="contentFilters">
+			<!-- <p class="text-price">Precio</p> -->
 			<!-- <v-layout row>
 				<v-flex shrink class="input-number-price-min">
 				<v-text-field
@@ -56,45 +56,16 @@
 			class="field"
 			type="text"
 			v-model="filters.price" /> -->
-			<v-layout mt-2 mb-2>
+			<v-layout mt-2 mb-2 v-for="(attr, indexAttr) in attributes" :key="indexAttr">
 				<app-select 
-				item-text="name"
-				item-value="id"
-				class="input-filter" 
-				placeholder="Marca"
-				v-model="filters.brand"/>
-			</v-layout>
-			<v-layout mt-2 mb-2>
-				<app-select 
-				item-text="name"
-				item-value="id"
-				class="input-filter" 
-				placeholder="Modelo"
-				v-model="filters.model"/>
-			</v-layout>
-			<v-layout mt-2 mb-2>
-				<app-select 
-				item-text="name"
-				item-value="id"
-				class="input-filter" 
-				placeholder="Año"
-				v-model="filters.year"/>
-			</v-layout>
-			<v-layout mb-2>
-				<app-select 
-				item-text="name"
-				item-value="id"
-				class="input-filter" 
-				placeholder="Lado"
-				v-model="filters.side"/>
-			</v-layout>
-			<v-layout mb-2>
-				<app-select 
-				item-text="name"
-				item-value="id"
-				class="input-filter" 
-				placeholder="Tipo"
-				v-model="filters.type"/>
+					returnObject
+					item-text="name"
+					class="input-filter"
+					:placeholder="attr.name"
+					:items="attr.attributeDetail"
+					:value="filters[attr.name]"
+					@input="setAttributes"
+				/>
 			</v-layout>
 		</div>
 	</div>
@@ -119,18 +90,15 @@ function openFilters() {
 	this.openUp = true;
 }
 
+function setAttributes(attr) {
+	const { name, code } = attr;
+	this.filters[name] = code;
+	this.$emit('attributes', code);
+}
+
 function data() {
 	return {
-		filters: {
-			brand: '',
-			model: '',
-			side: '',
-			price: '',
-			priceMin: 7,
-			priceMax: 60,
-			type: null,
-			year: null,
-		},
+		filters: {},
 		arrowUp: {
 			image: 'https://s3.amazonaws.com/apprunn-acl/COM-PRU-01/ARQ88/image/arrow-down-sign-to-navigate.svg',
 			name: 'up',
@@ -153,7 +121,6 @@ function data() {
 }
 
 export default {
-	data,
 	name: 'filters-category',
 	computed: {
 		...mapGetters([
@@ -161,14 +128,22 @@ export default {
 			'token',
 		]),
 	},
-	methods: {
-		closeCategory,
-		openFilters,
-	},
 	components: {
 		appInput,
 		appSelect,
 		buttonImage,
+	},
+	data,
+	methods: {
+		closeCategory,
+		openFilters,
+		setAttributes,
+	},
+	props: {
+		attributes: {
+			type: Array,
+			required: true,
+		},
 	},
 };
 </script>
