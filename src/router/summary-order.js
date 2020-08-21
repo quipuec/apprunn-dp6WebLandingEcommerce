fb704b3f983ad5308136c21a229022b6df68a28d
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-08-21 12:21:01 -0500

    feat(INT): observaciones

--- src/router/summary-order.js
+++ src/router/summary-order.js
@@ -1,6 +1,6 @@
 const route = {
 	name: 'buy-summary',
-	path: '/resumen-de-mi-pedido',
+	path: '/resumen-de-mi-pedido/:orderId?',
 	component: () => import(/* webpackChunkName: "summaryOrder" */ '@/pages/page-new-summary-order'),
 	beforeEnter: (to, from, next) => {
 		if (to.fullPath.includes('resumen-de-mi-pedido') && localStorage.getItem('ecommerce::token')) {
