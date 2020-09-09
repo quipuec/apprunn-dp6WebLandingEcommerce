421a406422e39c34f34eaea6ddf1239702060a5b
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-09-08 19:10:10 -0500

    feat(INT): terminos y condiciones + muestro imagen en detalle y carrito

--- src/components/order/paymentsMethods/place-to-pay.vue
+++ src/components/order/paymentsMethods/place-to-pay.vue
@@ -6,7 +6,7 @@
 			v-model="checkbox"
 		>
 		</v-checkbox>
-		<span>Acepto los <a :href="conditionsAndTermsLink" target="_blank">términos y condiciones</a></span>
+		<span>Acepto los <button type="button" @click="conditionsAndTermsLink">términos y condiciones</button></span>
 	</div>
 	<div class="placetopay-styles-container">
 		<button
@@ -25,15 +25,22 @@ import { mapState } from 'vuex';
 
 function conditionsAndTermsLink() {
 	const findIt = this.help.find((h) => {
-		debugger;
-		const name = h.name.toLowerCase().normalize('NFD')
-			.replace(/([aeio])\u0301|(u)[\u0301\u0308]/gi, '$1$2')
-			.normalize();
+		const name = this.normalize(h.name);
 		return name === 'terminos y condiciones';
 	});
-	debugger;
-	return findIt;
+	const [item] = findIt.section;
+	const link = findIt;
+	const newItem = item.name.split(' ').join('-');
+	const newLink = link.name.split(' ').join('-');
+	this.$router.push(`/ayuda/apartado/${newItem}/seccion/${newLink}`);
 }
+
+function normalize(str) {
+	return str.toLowerCase().normalize('NFD')
+		.replace(/([aeio])\u0301|(u)[\u0301\u0308]/gi, '$1$2')
+		.normalize();
+}
+
 async function linkGenerator() {
 	const url = `payment-gateway/${this.code}/checkout`;
 	const body = {
@@ -44,10 +51,8 @@ async function linkGenerator() {
 		uri: this.uri,
 	};
 	try {
-		await this.$httpSales.post(url, body);
-		this.showNotification('Se generó un enlace de pagos de forma exitosa', 'success');
-		this.$router.push({ name: 'buy-summary' });
-		this.$store.dispatch('GET_ORDER_INFO', { context: this, id: this.orderId });
+		const { data: response } = await this.$httpSales.post(url, body);
+		window.open(response.data.url, '_self');
 	} catch (error) {
 		this.showNotification(
 			'Ocurrió un error al generar el enlace de pago. Recargue y vuelva a intentarlo por favor',
@@ -70,11 +75,12 @@ export default {
 		...mapState({
 			help: state => state.commerce.helperCenter,
 		}),
-		conditionsAndTermsLink,
 	},
 	data,
 	methods: {
+		conditionsAndTermsLink,
 		linkGenerator,
+		normalize,
 	},
 	props: {
 		categoryCode: {
